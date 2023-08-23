a1cda786b6ba11937cd499ef2fb73a87827f93b2
Vaibhav436-prog <tyagivaibhav202@gmail.com>
2023-08-23 22:40:26 +0530

    completed chat app

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { registerUser, authUser, allUsers } = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', authUser);
+router.get('/', protect, allUsers);
+
+module.exports = router
